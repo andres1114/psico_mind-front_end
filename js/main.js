0aef22137f84dcfa025be3b7f5e9f87a9d19d053
Andres Zapata <polako_1114@hotmail.com>
2022-11-12 20:36:36 -0500

    PM-T-03 Adds logic to show matching coincidences from twitter

--- js/main.js
+++ js/main.js
@@ -28,11 +28,39 @@ function buscar(texto,red){
         data: objetoDeBusquedaJSON,
         dataType: "json",
         success: function(objetoDeRespuestaJSON){
-            //
+            showMatchingUsers({
+                responseObject: objetoDeRespuestaJSON
+            });
         },
         complete: function(){
             //
         }
 
     });
+}
+
+function showMatchingUsers(args) {
+    let tableNode = $("#table-1");
+    let tbodyNode = tableNode.find("tbody");
+
+    tbodyNode.empty();
+    let matchingUserObject;
+
+    if (Array.isArray(args.responseObject.response.rrss.twitter.users)) {
+        matchingUserObject = args.responseObject.response.rrss.twitter.users;
+    } else {
+        matchingUserObject = [];
+        matchingUserObject.push(args.responseObject.response.rrss.twitter.users);
+    }
+
+    let trNodeAray = [];
+    for (let x = 0; x < matchingUserObject.length; x++) {
+        let trNode = $("<tr>").attr({"class": "clickeable"});
+        let thNode = $("<th>").html("<img src=\"img/pngtree-user-mysterious-anonymous-account-vector-png-image_7997894.png\" height=\"40px\"/>");
+        let tdNode = $(`<td data-user-id="${matchingUserObject[x].data.id}">`).html(matchingUserObject[x].data.name + " (@" + matchingUserObject[x].data.username + ")");
+        trNode.append([thNode,tdNode]);
+        trNodeAray.push(trNode);
+    }
+
+    tbodyNode.append(trNodeAray);
 }
